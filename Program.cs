a2842628ec250a6c8420ad3f031a92efc415224c
AxelLofberg <axellofbergj@gmail.com>
2024-01-23 15:16:11 +0100

    spel test

--- Program.cs
+++ Program.cs
@@ -1,29 +1,98 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using System;
-using System.Text;
 
-namespace ConsoleApp
+
+
+namespace GuessingGameApi
 {
-    class Program
+    public class Startup
     {
-        static void Main()
+        public void ConfigureServices(IServiceCollection services)
         {
-            Console.OutputEncoding = Encoding.UTF8;
-            Console.InputEncoding = Encoding.UTF8;
-
-            Console.WriteLine("Tryck på Enter för att utföra åtgärden.");
-            Console.ReadLine();
-
-            Knapp();
+            // Konfigurera tjänster här
         }
 
-        static void Knapp()
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            Console.WriteLine("Åtgärnden utförs...");
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
 
-            Console.WriteLine("Ange ditt namn:");
-            string userName = Console.ReadLine();
+            app.UseRouting();
 
-            Console.WriteLine($"Hej {userName}! Logiken är nu utförd.");
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Välkommen till Gissningsspel API!");
+                });
+
+                endpoints.MapPost("/start", async context =>
+                {
+                    // Skapa ett slumpmässigt tal mellan 1 och 100
+                    Random random = new Random();
+                    int secretNumber = random.Next(1, 101);
+
+                    // Spara det slumpmässiga talet i sessionen
+                    context.Session.SetInt32("SecretNumber", secretNumber);
+
+                    await context.Response.WriteAsync("Gissningsspelet har startat. Gissa ett tal mellan 1 och 100.");
+                });
+
+                endpoints.MapPost("/guess", async context =>
+                {
+                    // Hämta det slumpmässiga talet från sessionen
+                    int? secretNumber = context.Session.GetInt32("SecretNumber");
+
+                    if (secretNumber.HasValue)
+                    {
+                        string requestBody = await new StreamReader(context.Request.Body).ReadToEndAsync();
+                        if (int.TryParse(requestBody, out int userGuess))
+                        {
+                            if (userGuess == secretNumber.Value)
+                            {
+                                await context.Response.WriteAsync("Grattis! Du har gissat rätt.");
+                            }
+                            else if (userGuess < secretNumber.Value)
+                            {
+                                await context.Response.WriteAsync("För lågt. Försök igen!");
+                            }
+                            else
+                            {
+                                await context.Response.WriteAsync("För högt. Försök igen!");
+                            }
+                        }
+                        else
+                        {
+                            await context.Response.WriteAsync("Var snäll och ange en giltig siffra.");
+                        }
+                    }
+                    else
+                    {
+                        await context.Response.WriteAsync("Spelet har inte startat. Anropa /start för att börja.");
+                    }
+                });
+            });
         }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
     }
 }
